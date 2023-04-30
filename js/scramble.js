90510a5eda77eac60282c7ab14540442bb52961a
Jamieee! <93412639+JamieFromNZ@users.noreply.github.com>
2023-04-30 15:17:04 +1200

    Made scramble generation

--- js/scramble.js
+++ js/scramble.js
@@ -1,1 +1,14 @@
-// Scramble generation functions for different cube types will be implemented here
+function generateScramble() {
+    const scrambler = new Scrambo();
+    const scramble = scrambler.get(1)[0];
+    displayScramble(scramble);
+}
+
+function displayScramble(scramble) {
+    const scrambleElement = document.getElementById('scramble');
+    scrambleElement.textContent = scramble;
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    generateScramble();
+});
